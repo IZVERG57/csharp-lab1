3b3b2c6a2bc0757a0534406ebc3bce452701e7b5
Alex <sawz1617@gmail.com>
2026-04-22 16:53:16 +0300

    Refactor project structure to 2 files

--- AbstractFactoryThemes/UiComposition.cs
+++ AbstractFactoryThemes/UiComposition.cs
@@ -1,4 +1,53 @@
-﻿namespace AbstractFactoryThemes;
+namespace AbstractFactoryThemes;
+
+public sealed class Application
+{
+    private readonly IGuiFactory _factory;
+    private IButton? _button;
+    private ICheckbox? _checkbox;
+
+    public Application(IGuiFactory factory)
+    {
+        _factory = factory;
+    }
+
+    public void CreateUi()
+    {
+        _button = _factory.CreateButton();
+        _checkbox = _factory.CreateCheckbox();
+    }
+
+    public void Paint()
+    {
+        if (_button is null || _checkbox is null)
+        {
+            throw new InvalidOperationException("CreateUi must be called before Paint.");
+        }
+
+        _button.Render();
+        _checkbox.Render();
+    }
+}
+
+public interface IGuiFactory
+{
+    IButton CreateButton();
+    ICheckbox CreateCheckbox();
+}
+
+public sealed class LightThemeFactory : IGuiFactory
+{
+    public IButton CreateButton() => new LightButton();
+
+    public ICheckbox CreateCheckbox() => new LightCheckbox();
+}
+
+public sealed class DarkThemeFactory : IGuiFactory
+{
+    public IButton CreateButton() => new DarkButton();
+
+    public ICheckbox CreateCheckbox() => new DarkCheckbox();
+}
 
 public interface IButton
 {
